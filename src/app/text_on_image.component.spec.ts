ec61e2a5b47639f1c0e716f5e1e8ec35e86fda7b
Lexie Eagleson <EAGLEA3@nationwide.com>
2024-12-18 15:56:58 -0500

    update some references

--- src/app/text_on_image.component.spec.ts
+++ src/app/text_on_image.component.spec.ts
@@ -20,16 +20,16 @@ describe('TextOnImageComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'invoice-sheet'`, () => {
+  it(`should have as title 'text_on_image'`, () => {
     const fixture = TestBed.createComponent(TextOnImageComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invoice-sheet');
+    expect(app.title).toEqual('text_on_image');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(TextOnImageComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, invoice-sheet');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, text_on_image');
   });
 });
